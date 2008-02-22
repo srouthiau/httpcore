0b525a83d72c9293d1c55f0fe645157bf13c4336
Oleg Kalnichevski <olegk@apache.org>
2008-02-22 13:57:51 +0000

    HTTPCORE-148: Transfer as many bytes as possible at a time. This fix improves performance significantly (up to 5x)
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/branches/limewire_contrib@630209 13f79535-47bb-0310-9956-ffa450edef68

--- module-nio/src/main/java/org/apache/http/nio/entity/NFileEntity.java
+++ module-nio/src/main/java/org/apache/http/nio/entity/NFileEntity.java
@@ -95,9 +95,11 @@ public class NFileEntity extends AbstractHttpEntity implements ProducingNHttpEnt
 
         long transferred;
         if(useFileChannels && encoder instanceof FileContentEncoder) {
-            transferred = ((FileContentEncoder)encoder).transfer(fileChannel, idx, 100);
+            transferred = ((FileContentEncoder)encoder)
+                .transfer(fileChannel, idx, Long.MAX_VALUE);
         } else {
-            transferred = fileChannel.transferTo(idx, Long.MAX_VALUE, new ContentEncoderChannel(encoder));
+            transferred = fileChannel.
+                transferTo(idx, Long.MAX_VALUE, new ContentEncoderChannel(encoder));
         }
 
         if(transferred > 0)
